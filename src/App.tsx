ebda53a36910484f161da62f8cf4cdfb79be6c9d
Pritam Gain <pritamjob2021@gmail.com>
2023-11-07 03:09:04 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <div className="flex flex-col">
           <div className="flex items-center font-bold hover:underline text-xl cursor-pointer">
             <a
-              href="https://www.youtube.com"
+              href="https://github.com/Pritamgit2002/Kanban_Next"
               target="_blank"
               className="flex items-center text-2xl"
             >
